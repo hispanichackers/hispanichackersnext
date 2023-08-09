4e730c4e79b52f7e2d2fc95e5afa354aa88f6bbf
Leroy Rosales <leroyrosales@gmail.com>
2023-08-09 16:41:47 -0500

    Updates styling and layout of header nav component

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -3,36 +3,20 @@ import Image from 'next/image';
 import Menus from './Menus';
 import Link from 'next/link';
 
-const Section = styled.header`
-  position: relative;
-  z-index: 10;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: center;
-  background: ${({ theme }) => theme.backgroundContrast};
-  -webkit-box-shadow: 0 8px 6px -6px black;
-  -moz-box-shadow: 0 8px 6px -6px black;
-  box-shadow: 0 8px 6px -6px black;
-  padding: 1rem 6rem;
-  @media (min-width: 1024px) {
-    padding-bottom: 0;
-  }
-`;
-
 export default function Header(props) {
   return (
-    <Section>
-      <Link href={"/"}>
-        <Image
-          src='/static/images/logo.png'
-          alt='Hispanic Hacker Logo'
-          width={100}
-          height={100}
-        />
-      </Link>
-      <h1>Development</h1>
-      <Menus {...props} />
-    </Section>
+    <header className="bg-white flex flex-row border-b-2 border-[#277e7e] py-4">
+      <section className="container mx-auto flex flex-col md:flex-row items-center gap-24">
+        <Link href={"/"}>
+          <Image
+            src='/static/images/logo.png'
+            alt='Hispanic Hacker Logo'
+            width={100}
+            height={100}
+          />
+        </Link>
+        <Menus {...props} />
+      </section>
+    </header>
   );
 }
