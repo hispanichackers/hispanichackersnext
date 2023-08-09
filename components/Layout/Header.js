407d8d8cbca61861f70bba47e17f0527002ec530
Leroy Rosales <leroyrosales@gmail.com>
2023-08-09 16:43:59 -0500

    Reduce gap for mobile

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -6,7 +6,7 @@ import Link from 'next/link';
 export default function Header(props) {
   return (
     <header className="bg-white flex flex-row border-b-2 border-[#277e7e] py-4">
-      <section className="container mx-auto flex flex-col md:flex-row items-center gap-24">
+      <section className="container mx-auto flex flex-col md:flex-row items-center md:gap-24">
         <Link href={"/"}>
           <Image
             src='/static/images/logo.png'
