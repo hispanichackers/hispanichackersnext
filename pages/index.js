3cc5c0b04206bba8bfea8487f600b96a9a2fe844
Leroy Rosales <leroyrosales@gmail.com>
2023-08-29 09:48:56 -0500

    Adds Links section to index.js

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Layout from '../components/Layout';
 import Intro from '../components/Home/Intro';
 import Mission from '../components/Home/Mission';
 import ContactButton from '../components/lib/ContactButton';
+import Links from '../components/Links';
 
 export default function IndexPage(props) {
   return (
     <Layout {...props}>
       <Intro />
       <Mission />
+      <Links/>
       <ContactButton />
     </Layout>
   );
