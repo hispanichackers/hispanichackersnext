2c0dc7d0ca45660bdb2a3070dd7f305b5cdcce87
Leroy Rosales <leroyrosales@gmail.com>
2023-09-04 08:16:48 -0500

    Removes ref to button styled component

--- components/Events/YearPicker.js
+++ components/Events/YearPicker.js
@@ -4,8 +4,8 @@ export default function YearPicker({ setYear }) {
   return (
     <>
       <hr />
-      <Button onClick={() => setYear('2022')}>2022</Button>
-      <Button onClick={() => setYear('2021')}>2021</Button>
+      <div onClick={() => setYear('2022')}>2022</div>
+      <div onClick={() => setYear('2021')}>2021</div>
       <hr />
     </>
   );
