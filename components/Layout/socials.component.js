9a9a77fa6139e87d276edcb3f4c400e83a580706
Leroy Rosales <leroyrosales@gmail.com>
2023-09-04 07:54:11 -0500

    Cleans up styles of social icons

--- components/Layout/socials.component.js
+++ components/Layout/socials.component.js
@@ -13,7 +13,7 @@ export default function Socials() {
   const { socialsHH } = globals();
   return (
     <>
-      <div className="flex flex-row gap-4 [&>a>svg]:w-8 [&>a>svg]:h-8 [&>a>svg:hover]:scale-125">
+      <div className="flex flex-row gap-8 [&>a>svg]:w-12 [&>a>svg]:h-12 [&>a>svg:hover]:scale-125 text-center">
         <a
           href={socialsHH.discord.url}
           alt={socialsHH.discord.alt}
@@ -26,38 +26,35 @@ export default function Socials() {
           alt={socialsHH.meetup.alt}
           target="_blank"
         >
-          <FaMeetup title={socialsHH.meetup.alt} />
-        </a>
-        <a href={socialsHH.slack.url} alt={socialsHH.slack.alt} target="_blank">
-          <FaSlackHash title={socialsHH.slack.alt} />
+          <FaMeetup title={socialsHH.meetup.alt} className="fill-red" />
         </a>
         <a
           href={socialsHH.youtube.url}
           alt={socialsHH.youtube.alt}
           target="_blank"
         >
-          <FaYoutube title={socialsHH.youtube.alt} />
+          <FaYoutube title={socialsHH.youtube.alt} className="fill-red" />
         </a>
         <a
           href={socialsHH.instagram.url}
           alt={socialsHH.instagram.alt}
           target="_blank"
         >
-          <FaInstagram title={socialsHH.instagram.alt} />
+          <FaInstagram title={socialsHH.instagram.alt} className="instagram-icon" />
         </a>
         <a
           href={socialsHH.facebook.url}
           alt={socialsHH.facebook.alt}
           target="_blank"
         >
-          <FaFacebookSquare title={socialsHH.facebook.alt} />
+          <FaFacebookSquare title={socialsHH.facebook.alt} className="fill-blue-500" />
         </a>
         <a
           href={socialsHH.linkedin.url}
           alt={socialsHH.linkedin.alt}
           target="_blank"
         >
-          <FaLinkedin title={socialsHH.linkedin.alt} />
+          <FaLinkedin title={socialsHH.linkedin.alt} className="fill-cyan"  />
         </a>
       </div>
     </>
