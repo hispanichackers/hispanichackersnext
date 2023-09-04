fad5287e47f458c9588ea87f1f5c0536e5f72c80
Leroy Rosales <leroyrosales@gmail.com>
2023-09-04 08:15:02 -0500

    Removes more styled components refs

--- components/Links/GatherTown.js
+++ components/Links/GatherTown.js
@@ -1,19 +1,11 @@
-import styled from 'styled-components';
+
 import Image from 'next/image';
 import globals from '../../assets/GlobalData';
 
-const Section = styled.section`
-  h2 {
-    text-align: left;
-  }
-  a {
-    text-decoration: underline;
-    color: ${({ theme }) => theme.linkUnderline};
-  }
-`;
+
 export default function GatherTown() {
   return (
-    <Section>
+    <>
       <h2>GatherTown</h2>
       <hr />
       <p>Visit our virtual gathering hub for events!</p>
@@ -29,6 +21,6 @@ export default function GatherTown() {
         width={1280}
         height={713}
       />
-    </Section>
+    </>
   );
 }
