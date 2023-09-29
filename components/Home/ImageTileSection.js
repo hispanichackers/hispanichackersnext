61eae735122626ddb2fc29188b726134afa09576
Leroy Rosales <leroyrosales@gmail.com>
2023-09-29 14:54:52 -0500

    Adds adjustment to top card design

--- components/Home/ImageTileSection.js
+++ components/Home/ImageTileSection.js
@@ -1,29 +1,31 @@
 export default function ImageTileSection() {
     return (
       <section className="bg-cyan flex flex-col pt-20 lg:grid lg:grid-cols-2">
-        <div className="h-96 lg:col-span-2 lg:text-center lg:h-96 lg:mt-20">
-        <img
-              className=" h-80 mb-5 rounded-2xl lg:w-auto "
+        <div className="lg:col-span-2 lg:text-center lg:mt-20 container mx-auto pb-8 px-4 md:px-0">
+        <figure class="grid isolate group">
+          <img
+              className="col-span-full row-span-full w-full max-h-[500px] mb-5 rounded-2xl object-cover object-[0_90%]"
               src="/static/images/capitalFactoryGroupPic.jpg"
               alt="Group pic"
             />
+            <figcaption className="col-span-full row-span-full mt-auto bg-red bg-opacity-50 p-8 opacity-0 group-hover:opacity-100 transition-opacity duration-500">Lorem ipsum</figcaption>
+        </figure>
         </div>
-        <div className="h-80">
+        <div className="">
         <img
-              className="h-72 mx-20 rounded-2xl"
+              className="mx-20 rounded-2xl"
               src="/static/images/nathanAndAndres.png"
               alt="Nathan and Andres"
             />
         </div>
-        <div className="h-72">
+        <div className="">
         <img
-              className="h-52 mx-2 rounded-2xl"
+              className="mx-2 rounded-2xl"
               src="/static/images/socialMeetup.png"
               alt="Meetup"
             />
-        
+
         </div>
       </section>
     );
   }
-  
